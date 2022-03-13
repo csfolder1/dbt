279523736eea62a770d2cfb2a6fe14179080dc0b
CSFolder <101512116+csfolder1@users.noreply.github.com>
2022-03-13 21:03:44 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,10 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <p>
+          <p>Databases can be classified in a variety of ways, but the most common classification is by the level of abstraction.</p> 
+         <p> There are four major types of databases: Relational, Network, Hierarchical, and Object-Oriented. In this blog post, </p>
+           we'll discuss each type in detail. Stay tuned for future posts on specific database applications!<a href="https://databasetown.com/">Types of Database</a>
         </p>
       </main>
 
